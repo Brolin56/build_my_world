3e3927fbf183be164243fecb503c8d5c87f369ad
brolin <a.brolin@hitechroboticsystemz.com>
2019-05-09 20:16:59 +0530

    First simulation world with mobile robot model

---
+++ build/CMakeFiles/description.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/description.dir/script/description.cpp.o"
+  "libdescription.pdb"
+  "libdescription.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/description.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
